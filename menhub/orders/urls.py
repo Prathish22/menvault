970b46a37190624fb9a2cdbaec2632a1f01588a4
Prathish22 <prthznull@gmail.com>
2025-01-30 23:05:06 +0530

    cart section

--- menhub/orders/urls.py
+++ menhub/orders/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('cart',views.show_cart,name='cart')
+    path('cart',views.show_cart,name='cart'),
+    path('add_tocart',views.add_tocart,name='add_tocart')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
